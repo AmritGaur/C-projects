45e89d23826777b35702c1c9f747e9e4cb13e616
AmritGaur <84451762+AmritGaur@users.noreply.github.com>
2021-06-04 22:32:09 +0530

    Add files via upload

---
+++ ifelse.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   int a = 238903;
+   int b = 2;
+   if(a%b == 0)
+   {printf("a is even");
+   }
+   else if (a%b!=0)
+   {printf("a is odd");
+   }
+}
